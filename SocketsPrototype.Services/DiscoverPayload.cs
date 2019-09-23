d75f1704ecd704cb280904da965f6dd4aa245eda
Ken Berry <rosskberry@gmail.com>
2019-09-22 23:27:33 -0600

    Remove SocketHelpers from dependencies

--- SocketsPrototype.Services/DiscoverPayload.cs
+++ SocketsPrototype.Services/DiscoverPayload.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using SocketHelpers.Discovery;
 
 namespace SocketsPrototype.Services
 {
-    class DiscoverPayload : IDiscoveryPayload
+    class DiscoverPayload
     {
         public string RemoteAddress { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public int RemotePort { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
